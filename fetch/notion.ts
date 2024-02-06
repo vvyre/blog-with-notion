f885b11950462c981f96870a830e192be26912f5
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-06 19:51:34 +0900

    feat: notion 통신 메서드 작성

---
+++ fetch/notion.ts
@@ -0,0 +1,45 @@
+import { Client } from '@notionhq/client';
+
+const notion = new Client({ auth: process.env.NOTION_KEY as string });
+export const getBlogData = async () =>
+  await notion.databases.query({
+    database_id: process.env.NOTION_BLOG_ID as string,
+    sorts: [
+      {
+        property: 'date',
+        direction: 'descending',
+      },
+    ],
+    filter: {
+      and: [
+        {
+          property: 'published',
+          checkbox: {
+            equals: true,
+          },
+        },
+      ],
+    },
+  });
+
+export const getPostMetaData = async (id: string) => {
+  return await notion.pages.retrieve({ page_id: id });
+};
+
+export const getPost = async (id: string) => {
+  const query = {
+    block_id: id,
+    page_size: 100,
+  };
+  let results = [];
+  let blocks = await notion.blocks.children.list(query);
+  results = [...blocks.results];
+  while (blocks.has_more) {
+    blocks = await notion.blocks.children.list({
+      ...query,
+      start_cursor: blocks.next_cursor as string | undefined,
+    });
+    results = [...results, ...blocks.results];
+  }
+  return results;
+};
