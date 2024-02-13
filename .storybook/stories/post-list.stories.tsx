2292194c93ee8bf21674a9711f9521697ba31a35
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-13 19:01:56 +0900

    test: add mock data

--- .storybook/stories/post-list.stories.tsx
+++ .storybook/stories/post-list.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import { PostList } from '../../_lib/components/compounds/postlist/post-list';
+import { PageDataRow } from '@/_lib/types/notion-response';
 
 const meta: Meta<typeof PostList> = {
   title: 'Compound/PostList',
@@ -16,6 +17,57 @@ const meta: Meta<typeof PostList> = {
 export default meta;
 type Story = StoryObj<typeof PostList>;
 
+const mockData: PageDataRow = {
+  object: 'page',
+  id: '123',
+  created_time: '',
+  last_edited_time: '',
+  created_by: {},
+  last_edited_by: {},
+  cover: null,
+  icon: null,
+  parent: {},
+  archived: false,
+  properties: {
+    date: {
+      date: { start: null, end: null, time_zone: null },
+    },
+    title: {
+      title: [
+        {
+          rich_text: {
+            type: 'text',
+            text: { content: 'abcd', link: null },
+            plain_text: 'abcd',
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: 'default',
+            },
+            href: null,
+          },
+          plain_text: 'abcd',
+        },
+      ],
+    },
+    summary: {
+      rich_text: [],
+    },
+    published: true,
+    thumbnail: {
+      thumbnail: [],
+    },
+    tags: {
+      multi_select: [],
+    },
+  },
+  url: '',
+  public_url: null,
+};
+
 export const Primary: Story = {
-  render: () => <PostList pageData={{}} />,
+  render: () => <PostList pageData={mockData} />,
 };
