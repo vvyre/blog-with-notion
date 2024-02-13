d2669db68c3516a9e3499a48e52eb09ca2b41ea6
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-14 01:42:38 +0900

    feat: add Flex component

---
+++ _lib/components/layout/flex/flex.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import { ALIGN_VARIANT, BASE, DIRECTION_VARIANT, JUSTIFY_VARIANT, WRAP_VARIANT } from './flex.css';
+
+type FlexProps<T extends ElementType> = {
+  as?: T;
+  width?: 'fill' | 'fit';
+  flexDirection?: keyof typeof DIRECTION_VARIANT;
+  justifyContents: keyof typeof JUSTIFY_VARIANT;
+  alignItems?: keyof typeof ALIGN_VARIANT;
+  flexWrap?: keyof typeof WRAP_VARIANT;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+export function Flex<T extends ElementType>({
+  as,
+  width = 'fit',
+  flexDirection = 'row',
+  justifyContents,
+  alignItems = 'center',
+  flexWrap = 'wrap',
+  children,
+}: FlexProps<T>) {
+  const className = `${width === 'fill' && BASE} ${DIRECTION_VARIANT[flexDirection]} ${
+    JUSTIFY_VARIANT[justifyContents]
+  } ${ALIGN_VARIANT[alignItems]} ${WRAP_VARIANT[flexWrap]}`;
+  const Component = as || 'div';
+  return <Component className={className}>{children}</Component>;
+}
+
+export default Flex;
