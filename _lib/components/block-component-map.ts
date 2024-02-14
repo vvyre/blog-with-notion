1002844eb75189dc5e02e582f5f6d13cec16e6f6
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-14 14:19:43 +0900

    refactor: component map 파일 분리

---
+++ _lib/components/block-component-map.ts
@@ -0,0 +1,42 @@
+import { BlockComponentMap } from '../types/components';
+import { BreadCrumb } from './block/breadcrumb/breadcrumb';
+import { NotionDivider } from './block/divider/divider';
+import { Heading_1 } from './block/headings/heading_1';
+import { Heading_2 } from './block/headings/heading_2';
+import { Heading_3 } from './block/headings/heading_3';
+import { Paragraph } from './block/paragraph/paragraph';
+import { Divider } from './layout/divider/Divider';
+
+export const blockComponentMap: BlockComponentMap = {
+  bookmark: Divider,
+  breadcrumb: BreadCrumb,
+  bulleted_list_item: Divider,
+  callout: Divider,
+  child_database: Divider,
+  child_page: Divider,
+  column: Divider,
+  column_list: Divider,
+  divider: NotionDivider,
+  embed: Divider,
+  equation: Divider,
+  file: Divider,
+  heading_1: Heading_1,
+  heading_2: Heading_2,
+  heading_3: Heading_3,
+  image: Divider,
+  link_preview: Divider,
+  link_to_page: Divider,
+  numbered_list_item: Divider,
+  paragraph: Paragraph,
+  pdf: Divider,
+  quote: Divider,
+  synced_block: Divider,
+  table: Divider,
+  table_of_contents: Divider,
+  table_row: Divider,
+  template: Divider,
+  to_do: Divider,
+  toggle: Divider,
+  unsupported: Divider,
+  video: Divider,
+};
--- _lib/components/block/Block.tsx
+++ _lib/components/block/Block.tsx
@@ -1,43 +1,7 @@
-import { Divider } from '../layout/divider/Divider';
 import { ComponentPropsWithoutRef } from 'react';
 import { BlockTypes, NotionBlock } from '@/_lib/types/block';
 import { BlockComponentMap } from '@/_lib/types/components';
-import { Paragraph } from './paragraph/paragraph';
-import { Heading_1 } from './headings/heading_1';
-
-const blockComponentMap: BlockComponentMap = {
-  bookmark: Divider,
-  breadcrumb: Divider,
-  bulleted_list_item: Divider,
-  callout: Divider,
-  child_database: Divider,
-  child_page: Divider,
-  column: Divider,
-  column_list: Divider,
-  divider: Divider,
-  embed: Divider,
-  equation: Divider,
-  file: Divider,
-  heading_1: Heading_1,
-  heading_2: Divider,
-  heading_3: Divider,
-  image: Divider,
-  link_preview: Divider,
-  link_to_page: Divider,
-  numbered_list_item: Divider,
-  paragraph: Paragraph,
-  pdf: Divider,
-  quote: Divider,
-  synced_block: Divider,
-  table: Divider,
-  table_of_contents: Divider,
-  table_row: Divider,
-  template: Divider,
-  to_do: Divider,
-  toggle: Divider,
-  unsupported: Divider,
-  video: Divider,
-};
+import { blockComponentMap } from '../block-component-map';
 
 type BlockProps<T extends BlockTypes> = {
   block: NotionBlock<T>;
