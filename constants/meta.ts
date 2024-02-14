0c6d998d0e8ab22111cb23496ca4f53bce6b5890
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-14 23:45:05 +0900

    feat: 사이트 메타데이터에서 외부 링크 부분을 스타일링으로 병합

--- constants/meta.ts
+++ constants/meta.ts
@@ -3,8 +3,8 @@ export const meta = {
   siteDiscription: '유승윤 개발 블로그',
   footer: {
     links: [
-      { name: 'Github↗︎', href: 'https://github.com/brewcoldblue' },
-      { name: 'Mail↗︎ seung.yoon.yu (at) gmail', href: 'mailto:seung.yoon.yu@gmail.com' },
+      { name: 'Github', href: 'https://github.com/brewcoldblue' },
+      { name: 'Contact - seung.yoon.yu (at) gmail', href: 'mailto:seung.yoon.yu@gmail.com' },
     ],
     copyright: 'Copyright 2024. Seungyoon Yu All rights reserved.',
   },
