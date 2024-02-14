8beb2a5e3216d87b79b6ad0f4e076d16bfd4f622
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-14 12:51:13 +0900

    feat: header/footer metadata 추가

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,4 +1,15 @@
 export const meta = {
   siteTitle: '유승윤 개발 블로그',
   siteDiscription: '유승윤 개발 블로그',
+  footer: {
+    links: [
+      { name: 'Github↗︎', href: 'https://github.com/brewcoldblue' },
+      { name: 'Mail↗︎ seung.yoon.yu (at) gmail', href: 'mailto:seung.yoon.yu@gmail.com' },
+    ],
+    copyright: 'Copyright 2024. Seungyoon Yu All rights reserved.',
+  },
 };
+export const routes: { name: string; path: string }[] = [
+  { name: 'blog', path: '/' },
+  { name: 'about', path: '/about' },
+];
