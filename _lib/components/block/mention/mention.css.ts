68a51d36bea73b02c1ce3b5793c1e9680241b8b6
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-16 16:12:30 +0900

    feat: 기존 a/link의 css mention으로 이관

---
+++ _lib/components/block/mention/mention.css.ts
@@ -0,0 +1,22 @@
+import { THEMES } from '@/_lib/styles/colors.css';
+import { jetbrainsMono } from '@/_lib/styles/fonts.css';
+import { style } from '@vanilla-extract/css';
+
+export const BASE = style({
+  fontFeatureSettings: '"case"',
+  display: 'block',
+  marginTop: '0.25rem',
+  marginBottom: '0.25rem',
+  padding: '0.5rem 0.75rem 0.5rem 0.75rem',
+  borderRadius: '0.5rem',
+  backgroundColor: THEMES.light_b2,
+  fontSize: '0.8rem',
+  selectors: {
+    '&:after': {
+      fontSize: '1rem',
+      marginLeft: '0.25rem',
+      fontFamily: jetbrainsMono,
+      content: '↗︎',
+    },
+  },
+});
