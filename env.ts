1ecbdc1e5c4a2f384601460f4b3e0b375bd64b22
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-18 18:47:55 +0900

    chore: env.ts 설정 변수 파일 추가

---
+++ env.ts
@@ -0,0 +1,6 @@
+export const notion_env = {
+  api_key: process.env.NOTION_KEY || '',
+  database_id: process.env.DATABASE_ID || '',
+  about_id: process.env.NOTION_ABOUT_ID || '',
+  api_root: process.env.NEXT_PUBLIC_API_ROOT || '',
+};
