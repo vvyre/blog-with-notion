85e56cba3c58471b516daf5e2d443751af4c38f8
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-19 19:12:40 +0900

    feat: 사이트 레이아웃 수정

--- _lib/components/template/responsive.css.ts
+++ _lib/components/template/responsive.css.ts
@@ -2,14 +2,14 @@ import { style } from '@vanilla-extract/css';
 
 export const RESPONSIVE = style({
   margin: '0 auto',
-  padding: '0 2.5rem 0 2.5rem',
+  padding: '0 1.5rem 0 1.5rem',
   width: '100vw',
   maxWidth: '60rem',
 });
 
 export const RESPONSIVE_POST = style({
   margin: '5rem auto',
-  padding: '0 2.5rem 0 2.5rem',
+  padding: '0 1.5rem 0 1.5rem',
   width: '100vw',
-  maxWidth: '50rem',
+  maxWidth: '48rem',
 });
