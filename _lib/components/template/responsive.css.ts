bebeb635fb0528cc5ac1470b1ca3270af3bc32b8
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-19 11:09:43 +0900

    feat: 양옆 여백 추가

--- _lib/components/template/responsive.css.ts
+++ _lib/components/template/responsive.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const RESPONSIVE = style({
   //TODO: 반응형 설정
   margin: '0 auto',
+  padding: '0 1rem 0 1rem',
   width: '100vw',
   maxWidth: '60rem',
 });
@@ -10,6 +11,7 @@ export const RESPONSIVE = style({
 export const RESPONSIVE_POST = style({
   //TODO: 반응형 설정
   margin: '5rem auto',
+  padding: '0 1rem 0 1rem',
   width: '100vw',
-  maxWidth: '45rem',
+  maxWidth: '48rem',
 });
