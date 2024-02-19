38b95871ff949fdd4e124bb1132ff4b7bd5f4360
brewcoldblue <seung.yoon.yu@gmail.com>
2024-02-19 18:11:21 +0900

    chore: next api root 명시 불필요로 삭제

--- env.ts
+++ env.ts
@@ -2,5 +2,4 @@ export const notion_env = {
   api_key: process.env.NOTION_KEY || '',
   database_id: process.env.DATABASE_ID || '',
   about_id: process.env.NOTION_ABOUT_ID || '',
-  api_root: process.env.NEXT_PUBLIC_API_ROOT || '',
 };
