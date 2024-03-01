690796b2858a5056295d75da2282bb692b8ef3ee
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-01 17:00:03 +0900

    fix: enable plaiceholder

--- utils/get-blurred-img.ts
+++ utils/get-blurred-img.ts
@@ -5,14 +5,14 @@ export const getBlurredImg = async (block: GetBlockResponse) => {
   if ('image' in block) {
     const imgUrl = block.image.type === 'external' ? block.image.external?.url : block.image.file?.url;
 
-    // const buffer = await fetch(imgUrl as string).then(async res => Buffer.from(await res.arrayBuffer()));
-    // const { base64 } = await getPlaiceholder(buffer);
+    const buffer = await fetch(imgUrl as string).then(async res => Buffer.from(await res.arrayBuffer()));
+    const { base64 } = await getPlaiceholder(buffer);
 
     return {
       ...block,
       image: {
         ...block.image,
-        blurDataURL: '',
+        blurDataURL: base64,
       },
     };
   }
