14679bb43f6ee612cc6159481a0ca7b1cb2937e8
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-04 17:12:19 +0900

    fix: fix naver search advisor

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { meta } from '@/constants/meta';
 import type { Metadata } from 'next';
 import { View } from '@/_lib/components/layout/view/view';
 import { Footer } from '@/_lib/components/compounds/footer/footer';
-
+import Head from 'next/head';
 export const metadata: Metadata = {
   title: meta.siteTitle,
   description: meta.siteDiscription,
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Head>
+        <meta name="naver-site-verification" content="bae405a50bfce503cf32d8d3f8460931bc2bf9bd" />
+      </Head>
       <View as="body">
         <Header />
         <View as="main">{children}</View>
