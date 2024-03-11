60a2ae23876e89d55dd0ca7f0d76abeba587f48b
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-11 16:38:36 +0900

    feat: add list items' indentation

--- _lib/components/block/numbered-list-item/numbered-list-item.tsx
+++ _lib/components/block/numbered-list-item/numbered-list-item.tsx
@@ -5,17 +5,18 @@ import { Txt } from '../../typography/txt/txt';
 import { BASE, DEPTH_1 } from './numbered-list-item.css';
 import type { NotionComponentPropsWithChildren } from '@/_lib/types/component-common';
 import type { RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints';
+import { Flex } from '../../layout/flex/flex';
 
 export function NumberedListItem({ block }: NotionComponentPropsWithChildren<'numbered_list_item'>) {
   const id = useId();
   return (
     <List as="li" styleVariant={`${BASE} ${DEPTH_1}`}>
-      <div>
+      <Flex flexDirection="column" justifyContents="flexStart" alignItems="flexStart">
         {block.numbered_list_item.rich_text.map((txt: RichTextItemResponse) => (
           <Txt key={id} as="span" richText={txt} />
         ))}
         {block.numbered_list_item.children && <ChildrenBlocks block={block} />}
-      </div>
+      </Flex>
     </List>
   );
 }
