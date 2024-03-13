6e96f53738f5ad129af375e8a0d364eaf4057e65
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-13 18:51:34 +0900

    chore: migrate npm to pnpm and delege sharp to resolve vercel build issue

--- utils/get-blurred-img.ts
+++ utils/get-blurred-img.ts
@@ -1,17 +1,18 @@
 import type { GetBlockResponse } from '@notionhq/client/build/src/api-endpoints';
-import { getPlaiceholder } from 'plaiceholder';
+import loadingImg from '#/img/loading.svg';
+// import { getPlaiceholder } from 'plaiceholder';
 
 export const getBlurredImg = async (block: GetBlockResponse) => {
   if ('image' in block) {
     const imgUrl = block.image.type === 'external' ? block.image.external?.url : block.image.file?.url;
-    const buffer = await fetch(imgUrl as string).then(async res => Buffer.from(await res.arrayBuffer()));
-    const { base64 } = await getPlaiceholder(buffer);
+    // const buffer = await fetch(imgUrl as string).then(async res => Buffer.from(await res.arrayBuffer()));
+    // const { base64 } = await getPlaiceholder(buffer);
 
     return {
       ...block,
       image: {
         ...block.image,
-        blurDataURL: base64,
+        blurDataURL: loadingImg,
       },
     };
   }
