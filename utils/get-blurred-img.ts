d286e9644d35f4a8db10fa21e5255da4f6e559ad
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-13 18:47:07 +0900

    feat: activate plaiceholder

--- utils/get-blurred-img.ts
+++ utils/get-blurred-img.ts
@@ -1,17 +1,17 @@
 import type { GetBlockResponse } from '@notionhq/client/build/src/api-endpoints';
-import loadingImg from '#/img/loading.svg';
+import { getPlaiceholder } from 'plaiceholder';
 
 export const getBlurredImg = async (block: GetBlockResponse) => {
   if ('image' in block) {
-    // const imgUrl = block.image.type === 'external' ? block.image.external?.url : block.image.file?.url;
-    // const buffer = await fetch(imgUrl as string).then(async res => Buffer.from(await res.arrayBuffer()));
-    // const { base64 } = await getPlaiceholder(buffer);
+    const imgUrl = block.image.type === 'external' ? block.image.external?.url : block.image.file?.url;
+    const buffer = await fetch(imgUrl as string).then(async res => Buffer.from(await res.arrayBuffer()));
+    const { base64 } = await getPlaiceholder(buffer);
 
     return {
       ...block,
       image: {
         ...block.image,
-        blurDataURL: loadingImg,
+        blurDataURL: base64,
       },
     };
   }
