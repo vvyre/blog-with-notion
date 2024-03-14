2e69419cd39465e0279600b339ad2d211366e6dc
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-14 19:02:56 +0900

    fix: faq page의 block 처리 함수 최신화

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,19 +1,19 @@
 import { meta } from '@/constants/meta';
-import { getPost, getPostMetaData } from '@/fetch/notion';
+import { getPost } from '@/fetch/notion';
 import { Block } from '@/_lib/components/block/block';
 import { Article } from '@/_lib/components/layout/post/article';
 import { Spacing } from '@/_lib/components/layout/spacing/spacing';
-import { groupedBlocks } from '@/utils/grouped-blocks';
-import { Txt } from '@/_lib/components/typography/txt/txt';
 import { View } from '@/_lib/components/layout/view/view';
 import { POST_GRID, POST_GRID_PLACEMENT } from '@/_lib/components/layout/post/article.css';
 import { BACK_BUTTON_PLACEMENT } from '@/_lib/components/layout/post-title/post-title.css';
 import { BackButton } from '@/_lib/components/interaction/back-button/back-button';
+import { notion_env } from '@/env';
+import { processedBlock } from '@/utils/process-block';
 
 export const revalidate = 3600;
 
 export default async function Post() {
-  const blocks = groupedBlocks(await getPost(process.env.NOTION_ABOUT_ID as string));
+  const blocks = await processedBlock(await getPost(notion_env.about_id));
 
   return (
     <>
