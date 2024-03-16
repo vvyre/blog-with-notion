aff5de9aeb3300bc7777898bb8725c13c1150a17
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-16 13:09:56 +0900

    feat: 간단한 프로필 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
-import { getPostList } from '@/fetch/notion';
+import { getPost, getPostList } from '@/fetch/notion';
 import { PostList } from '@/_lib/components/layout/post-list/post-list';
-import { List } from '@/_lib/components/list/list';
-import { PostListHeader } from '@/_lib/components/layout/post-list-header/post-list-header';
+import { Profile } from '@/_lib/components/layout/profile/profile';
 import { Txt } from '@/_lib/components/typography/txt/txt';
 import { View } from '@/_lib/components/layout/view/view';
 import { PostListBox } from '@/_lib/components/layout/post-list-box/post-list-box';
 import { Spacing } from '@/_lib/components/layout/spacing/spacing';
+import { processedBlock } from '@/utils/process-block';
+import { notion_env } from '@/env';
+import { Block } from '@/_lib/components/block/block';
 
 export const revalidate = 300;
 
 export default async function PostListPage() {
   const postList = await getPostList();
+  const PROFILE_BLOCKS = await processedBlock(await getPost(notion_env.profile_id));
 
   return (
     <View as="main">
       <View as="section">
-        <PostListHeader postListData={postList}>전체 글 {postList.length}편</PostListHeader>
+        <Profile>
+          {PROFILE_BLOCKS.map(block => (
+            <Block block={block} />
+          ))}
+        </Profile>
         <PostListBox>
           {postList.length > 0 ? (
             postList.map(post => <PostList key={post.id} pageData={post} />)
