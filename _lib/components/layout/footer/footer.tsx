329b9d89c5bd69eb04c9175a93c27802b461d498
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-18 14:10:42 +0900

    feat: add rss link in footer

--- _lib/components/layout/footer/footer.tsx
+++ _lib/components/layout/footer/footer.tsx
@@ -3,11 +3,16 @@ import { BASE, FOOTER_COPYRIGHT } from './footer.css';
 import { Txt } from '../../typography/txt/txt';
 import { View } from '../view/view';
 import { Spacing } from '../spacing/spacing';
+import { site_env } from '@/env';
 
 export function Footer() {
   return (
     <View styleVariant={BASE}>
       <Spacing size="1rem" />
+      <Txt as="a" target="_blank" href={`${site_env.root}/feed.xml`} styleVariant={FOOTER_COPYRIGHT}>
+        rss
+      </Txt>
+      <Spacing size="0.5rem" />
       <Txt styleVariant={FOOTER_COPYRIGHT}>{meta.footer.copyright}</Txt>
     </View>
   );
