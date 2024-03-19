1ad4f6302aa2132ab7a4e7077aad164b4d4d94e0
brewcoldblue <seung.yoon.yu@gmail.com>
2024-03-19 18:46:21 +0900

    feat: turn off vercel img optimization

--- _lib/components/layout/logo/logo.tsx
+++ _lib/components/layout/logo/logo.tsx
@@ -5,7 +5,12 @@ import { LOGO } from './logo.css';
 export function Logo() {
   return (
     <Txt as="Link" href="/faq" styleVariant={LOGO}>
-      <Image src={logo} alt="frequently asked questions, 프로필" style={{ width: '1.75rem', height: '1.75rem' }} />
+      <Image
+        unoptimized
+        src={logo}
+        alt="frequently asked questions, 프로필"
+        style={{ width: '1.75rem', height: '1.75rem' }}
+      />
     </Txt>
   );
 }
