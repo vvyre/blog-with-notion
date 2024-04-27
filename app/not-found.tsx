4fb9c986c9980a634d8c777f37015084c1428fb0
brewcold <seung.yoon.yu@gmail.com>
2024-04-27 14:19:52 +0900

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Flex } from '@/_lib/components/layout/flex/flex';
+import { Article } from '@/_lib/components/layout/post/article';
+import { View } from '@/_lib/components/layout/view/view';
+import { Heading } from '@/_lib/components/typography/heading/heading';
+import { Txt } from '@/_lib/components/typography/txt/txt';
+import { useEffect } from 'react';
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Article>
+      <Flex justifyContents="center" alignItems="center">
+        <View>
+          <Heading as="h1">404 NOT FOUND</Heading>
+          <Txt as="Link" href="/">
+            돌아가기
+          </Txt>
+        </View>
+      </Flex>
+    </Article>
+  );
+}
