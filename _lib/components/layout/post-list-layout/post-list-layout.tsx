b8b6731f12351762ec5f0882e7269c44812dad51
brewcold <seung.yoon.yu@gmail.com>
2024-05-05 18:25:27 +0900

    feat: remove '전체' category

--- _lib/components/layout/post-list-layout/post-list-layout.tsx
+++ _lib/components/layout/post-list-layout/post-list-layout.tsx
@@ -9,13 +9,12 @@ import { Btn } from '../../interaction/button/btn';
 import { Flex } from '../flex/flex';
 import { Spacing } from '../spacing/spacing';
 
-export type Category = '전체' | '개발' | '신변잡기' | '감상';
-const categories = ['전체', '개발', '신변잡기', '감상'] as const;
+export type Category = '개발' | '신변잡기' | '감상';
+const categories = ['개발', '신변잡기', '감상'] as const;
 
 export function PostListLayout({ postList }: { postList: PostListObject }) {
-  const [selectedCategory, setCategory] = useState<Category>('전체');
+  const [selectedCategory, setCategory] = useState<Category>('개발');
   const POSTS: Record<Category, PostListObject> = {
-    전체: postList,
     개발: postList.filter(post => post.properties.tags.multi_select[0].color === 'purple'),
     신변잡기: postList.filter(post => post.properties.tags.multi_select[0].color === 'blue'),
     감상: postList.filter(post => post.properties.tags.multi_select[0].color === 'yellow'),
@@ -39,7 +38,7 @@ export function PostListLayout({ postList }: { postList: PostListObject }) {
       <Spacing size="3rem" />
       <View as="ul">
         {POSTS[selectedCategory].length > 0 ? (
-          postList.map(post => <PostList key={post.id} pageData={post} selectedCategory={selectedCategory} />)
+          postList.map(post => <PostList key={post.id} pageData={post} />)
         ) : (
           <Flex width="fill" justifyContents="center" alignItems="center">
             <Spacing size="10rem" />
--- _lib/components/layout/post-list/post-list.tsx
+++ _lib/components/layout/post-list/post-list.tsx
@@ -5,23 +5,20 @@ import { parsedSlug } from '@/utils/parsed-slug';
 import { Heading } from '../../typography/heading/heading';
 import { Txt } from '../../typography/txt/txt';
 import { getSummary } from '@/utils/get-summary';
-import { getTags } from '@/utils/get-tags';
+// import { getTags } from '@/utils/get-tags';
 import { Spacing } from '../spacing/spacing';
-import { POST_BOX, POST_CATEGORY, POST_LINK, POST_SUMMARY, POST_TITLE, RELEASED_DATE } from './post-list.css';
+import { POST_BOX, POST_LINK, POST_SUMMARY, POST_TITLE, RELEASED_DATE } from './post-list.css';
 import { getDate } from '@/utils/get-date';
 import { Flex } from '../flex/flex';
-import { Category } from '../post-list-layout/post-list-layout';
 
 interface PostListProps {
   pageData: GetPageResponse;
-  selectedCategory: Category;
 }
 
-export function PostList({ pageData, selectedCategory }: PostListProps) {
+export function PostList({ pageData }: PostListProps) {
   const URI = `/blog/${parsedSlug(pageData)}`;
-  const TAGS = getTags(pageData);
-  const CATEGORY = TAGS[0].name;
-  const IS_ALL_POST = selectedCategory === '전체';
+  // const TAGS = getTags(pageData);
+  // const CATEGORY = TAGS[0].name;
   const TITLE = getTitle(pageData);
   const SUMMARY = getSummary(pageData);
   const DATE = getDate(pageData);
@@ -30,19 +27,7 @@ export function PostList({ pageData, selectedCategory }: PostListProps) {
     <List as="li">
       <Flex justifyContents="center" styleVariant={POST_BOX}>
         <Txt as="Link" href={URI} styleVariant={POST_LINK}>
-          {IS_ALL_POST ? (
-            <Flex justifyContents="center" alignItems="center">
-              <Txt as="span" styleVariant={POST_CATEGORY}>
-                {CATEGORY}
-              </Txt>
-              <Spacing size="0.3rem" dir="hori" />
-              <Txt as="span" styleVariant={RELEASED_DATE}>
-                {DATE}
-              </Txt>
-            </Flex>
-          ) : (
-            <Txt styleVariant={RELEASED_DATE}>{DATE}</Txt>
-          )}
+          <Txt styleVariant={RELEASED_DATE}>{DATE}</Txt>
           <Heading as="h1" styleVariant={POST_TITLE}>
             {TITLE}
           </Heading>
