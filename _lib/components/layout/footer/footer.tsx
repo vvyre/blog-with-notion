b5d45041b7683aa792674ff2e1ce45bb96ef248f
brewcold <seung.yoon.yu@gmail.com>
2024-06-16 18:38:50 +0900

    delete deco

--- _lib/components/layout/footer/footer.tsx
+++ _lib/components/layout/footer/footer.tsx
@@ -1,14 +1,12 @@
 import { meta } from '@/constants/meta';
-import { BASE, FOOTER_COPYRIGHT, FOOTER_DECORATION, FOOTER_LINK, FOOTER_SITE_TITLE } from './footer.css';
+import { BASE, FOOTER_COPYRIGHT, FOOTER_LINK } from './footer.css';
 import { Txt } from '../../typography/txt/txt';
 import { View } from '../view/view';
-import { Spacing } from '../spacing/spacing';
 import { site_env } from '@/env';
 
 export function Footer() {
   return (
     <View styleVariant={BASE}>
-      <Txt styleVariant={FOOTER_DECORATION}>/</Txt>
       <Txt as="a" target="_blank" href={`${site_env.root}/feed.xml`} styleVariant={FOOTER_LINK}>
         RSS
       </Txt>
