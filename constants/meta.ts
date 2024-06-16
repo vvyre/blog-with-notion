27ee5803d0c8ba521623999f030141d6d4256ade
brewcold <seung.yoon.yu@gmail.com>
2024-06-16 18:40:28 +0900

    change copyright text

--- constants/meta.ts
+++ constants/meta.ts
@@ -20,6 +20,6 @@ export const meta = {
       { name: 'Github', href: 'https://github.com/brewcold' },
       { name: 'seung.yoon.yu (at) gmail', href: 'mailto:seung.yoon.yu@gmail.com' },
     ],
-    copyright: 'Copyright 2024. Seungyoon Yu All rights reserved.',
+    copyright: 'Copyright 2024. SEUNGYOON YU All rights reserved.',
   },
 };
