1a8649eb18a35b5c5fc36abfd9d1ca0a17a94132
brewcold <seung.yoon.yu@gmail.com>
2024-06-16 18:38:12 +0900

    add decoration and change design

--- _lib/components/layout/footer/footer.css.ts
+++ _lib/components/layout/footer/footer.css.ts
@@ -1,27 +1,38 @@
-import { THEMES } from '@/_lib/styles/colors.css';
+import { COLORS, THEMES } from '@/_lib/styles/colors.css';
 import { code } from '@/_lib/styles/fonts.css';
 import { style } from '@vanilla-extract/css';
 
 export const BASE = style([
   {
     marginTop: '2rem',
-    marginBottom: '5rem',
+    marginBottom: '1rem',
     textAlign: 'center',
     width: '100%',
   },
 ]);
 
 export const FOOTER_SITE_TITLE = style({
-  fontSize: '1rem',
+  fontSize: '0.75rem',
+  wordSpacing: '-0.25rem',
   fontWeight: 400,
 });
 
+export const FOOTER_DECORATION = style({
+  fontSize: '0.75rem',
+  cursor: 'default',
+});
+
 export const FOOTER_LINK = style({
-  fontSize: '0.85rem',
+  fontSize: '0.75rem',
   wordSpacing: '-0.25rem',
+  transition: 'color 0.15s',
   selectors: {
+    '&:hover': {
+      color: THEMES.highlight,
+    },
     '&:after': {
       marginLeft: '0.25rem',
+      marginRight: '0.75rem',
       fontSize: '0.85rem',
       fontFamily: code,
       content: '↗︎',
@@ -30,6 +41,7 @@ export const FOOTER_LINK = style({
 });
 
 export const FOOTER_COPYRIGHT = style({
-  fontSize: '0.8rem',
+  fontSize: '0.75rem',
+  wordSpacing: '-0.25rem',
   color: THEMES.gray,
 });
--- _lib/components/layout/footer/footer.tsx
+++ _lib/components/layout/footer/footer.tsx
@@ -1,5 +1,5 @@
 import { meta } from '@/constants/meta';
-import { BASE, FOOTER_COPYRIGHT, FOOTER_LINK, FOOTER_SITE_TITLE } from './footer.css';
+import { BASE, FOOTER_COPYRIGHT, FOOTER_DECORATION, FOOTER_LINK, FOOTER_SITE_TITLE } from './footer.css';
 import { Txt } from '../../typography/txt/txt';
 import { View } from '../view/view';
 import { Spacing } from '../spacing/spacing';
@@ -8,10 +8,10 @@ import { site_env } from '@/env';
 export function Footer() {
   return (
     <View styleVariant={BASE}>
+      <Txt styleVariant={FOOTER_DECORATION}>/</Txt>
       <Txt as="a" target="_blank" href={`${site_env.root}/feed.xml`} styleVariant={FOOTER_LINK}>
         RSS
       </Txt>
-      <Spacing dir="right" size="0.35rem" />
       <Txt as="a" target="_blank" href="mailto:seung.yoon.yu@gmail.com" styleVariant={FOOTER_LINK}>
         seung.yoon.yu (at) gmail
       </Txt>
