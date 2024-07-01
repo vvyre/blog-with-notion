0e83c956ca329f571eb2d392669bc4909f464e94
brewcold <seung.yoon.yu@gmail.com>
2024-07-01 16:04:39 +0900

    site env 변수명 카테고리별로 세분화

--- app/api/server-sitemap.xml/route.ts
+++ app/api/server-sitemap.xml/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   const engineeringPosts = await getCachedPostList(notion_env.database_id);
   const asdfPosts = await getCachedPostList(notion_env.asdf_id);
 
-  const engineeringSlugs = engineeringPosts.map(post => `${site_env.blog}${parsedSlug(post)}`);
+  const engineeringSlugs = engineeringPosts.map(post => `${site_env.engineering}${parsedSlug(post)}`);
   const asdfSlugs = asdfPosts.map(post => `${site_env.asdf}${parsedSlug(post)}`);
 
   const slugs = [...engineeringSlugs, ...asdfSlugs];
