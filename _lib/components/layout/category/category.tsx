0469632556105926268a784adcc72e3014013ff5
brewcold <seung.yoon.yu@gmail.com>
2024-07-01 16:10:42 +0900

    category 컴포넌트 작동 state 기반으로 변경

--- _lib/components/layout/category/category.tsx
+++ _lib/components/layout/category/category.tsx
@@ -1,14 +1,14 @@
 'use client';
-import { categories } from '@/constants/category';
-import { usePathname } from 'next/navigation';
+import { CategoryType, categories } from '@/constants/category';
 import { Btn } from '../../interaction/button/btn';
 import { CATEGORY } from './category.css';
 import { View } from '../view/view';
 import { LAYOUT_CENTERED } from '../article/article.css';
 import { Spacing } from '../spacing/spacing';
+import { useState } from 'react';
 
 export function Category() {
-  const current = usePathname();
+  const [selected, setSelected] = useState<CategoryType>('engineering');
   return (
     <View styleVariant={LAYOUT_CENTERED}>
       <Spacing size="2rem" />
@@ -17,7 +17,8 @@ export function Category() {
           as="Link"
           key={c.name}
           href={c.route}
-          styleVariant={current.includes(c.route) ? CATEGORY['selected'] : CATEGORY['options']}>
+          styleVariant={selected === c.type ? CATEGORY['selected'] : CATEGORY['options']}
+          onClick={() => setSelected(c.type)}>
           {c.name}
         </Btn>
       ))}
--- constants/category.ts
+++ constants/category.ts
@@ -1,5 +1,5 @@
 export type CategoryType = 'engineering' | 'asdf';
-export const categories = [
-  { name: '개발', route: '/' },
-  { name: '신변잡기', route: '/asdf' },
+export const categories: { name: string; route: string; type: CategoryType }[] = [
+  { name: '개발', route: '/', type: 'engineering' },
+  { name: '신변잡기', route: '/asdf', type: 'asdf' },
 ];
