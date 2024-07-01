82a5a135775a8797ffe810a3146fcb3bae3da5d4
brewcold <seung.yoon.yu@gmail.com>
2024-07-01 17:39:57 +0900

    뒤로 가기 버튼의 대상 URI가 현재 카테고리에 따라 동적으로 결정되도록 수정

--- _lib/components/interaction/button/other-articles-btn/other-articles-btn.tsx
+++ _lib/components/interaction/button/other-articles-btn/other-articles-btn.tsx
@@ -1,10 +1,12 @@
-import { styleVariants } from '@vanilla-extract/css';
+'use client';
 import { Btn } from '../btn';
 import { BASE } from './other-articles-btn.css';
+import { useCategory } from '@/_lib/react/use-category';
 
 export function OtherArticlesBtn() {
+  const [path] = useCategory();
   return (
-    <Btn as="Link" href="/" styleVariant={BASE}>
+    <Btn as="Link" href={path} styleVariant={BASE}>
       다른 글
     </Btn>
   );
