c028aaa73753eca3cdc533e8a890a3afca33ed75
brewcold <seung.yoon.yu@gmail.com>
2024-08-16 16:39:37 +0900

    사담 블로그 상수변수 삭제

--- env.ts
+++ env.ts
@@ -1,15 +1,12 @@
 export const notion_env = {
   api_key: process.env.NOTION_KEY || '',
   database_id: process.env.NOTION_BLOG_DATABASE_ID || '',
-  asdf_id: process.env.NOTION_ASDF_DATABASE_ID || '',
   about_id: process.env.NOTION_ABOUT_PAGE_ID || '',
-  profile_id: process.env.NOTION_PROFILE_PAGE_ID || '',
 };
 
 export const site_env = {
   root: process.env.NEXT_PUBLIC_ROOT || '',
   engineering: (process.env.NEXT_PUBLIC_ROOT || '') + '/engineering',
-  asdf: (process.env.NEXT_PUBLIC_ROOT || '') + '/asdf',
   gv: process.env.GOOGLE_SITE_VERIFICATION || '',
   nv: process.env.NAVER_SITE_VERIFICATION || '',
   utterances: process.env.NEXT_PUBLIC_UTTERANCES || '',
