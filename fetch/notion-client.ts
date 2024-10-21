86c5dbce18f021dddf00bce0f8fe0d9583e36d98
brewcold <seung.yoon.yu@gmail.com>
2024-10-21 15:22:42 +0900

    환경변수를 직접 참조하지 않도록 수정

--- fetch/notion-client.ts
+++ fetch/notion-client.ts
@@ -1,3 +1,5 @@
+import { notion_env } from '@/env';
 import { Client } from '@notionhq/client';
-const notionFetch = () => new Client({ auth: process.env.NOTION_KEY });
+const notionFetch = () => new Client({ auth: notion_env.api_key });
+
 export const notion: Client = notionFetch();
