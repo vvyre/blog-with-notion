b628b208ff90882c03393f0b895f75720926c63a
brewcold <seung.yoon.yu@gmail.com>
2024-11-05 17:36:34 +0900

    로고 닉네임을 ABOUT으로 변경

--- _lib/components/layout/logo/logo.tsx
+++ _lib/components/layout/logo/logo.tsx
@@ -1,11 +1,10 @@
 'use client';
 import { Txt } from '../../typography/txt/txt';
 import { LOGO } from './logo.css';
-import { meta } from '@/constants/meta';
 export function Logo() {
   return (
     <Txt as="Link" href="/" styleVariant={LOGO}>
-      {meta.siteTitle}
+      ABOUT
     </Txt>
   );
 }
