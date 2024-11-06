9eadb5eec86fa83f96c4551d6e0ddc10d2fea0c8
brewcold <seung.yoon.yu@gmail.com>
2024-11-06 18:16:38 +0900

    github repo에서 이미지를 가져올 수 있도록 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,17 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {
   images: {
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
         port: '',
         pathname: '/**',
       },
