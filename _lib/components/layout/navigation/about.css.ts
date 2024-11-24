70e4c527726c88768a5f00156680feec3a035a50
brewcold <seung.yoon.yu@gmail.com>
2024-11-24 14:56:42 +0900

    프로필 텍스트 가운데 정렬

--- _lib/components/layout/navigation/about.css.ts
+++ _lib/components/layout/navigation/about.css.ts
@@ -5,6 +5,8 @@ import { style } from '@vanilla-extract/css';
 export const ABOUT = style([
   {
     width: '100vw',
+    margin: '0 auto',
+    maxWidth: '50rem',
     wordBreak: 'keep-all',
     '@media': {
       '(0 <= width <= 768px)': {
