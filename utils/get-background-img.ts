68b17a0fe13ff3180e6ab2ef7ab7bca8210d0404
brewcold <seung.yoon.yu@gmail.com>
2024-11-26 14:50:30 +0900

    feat: 배경 사진 설정 로직 및 배경 사진 변경

--- utils/get-background-img.ts
+++ utils/get-background-img.ts
@@ -1,4 +1,5 @@
 export const getBackgroundImg = () => {
-  const num = Math.floor(Math.random() * 24);
+  const r = Math.random() * 26 - 0.00000000001;
+  const num = r < 0 ? 0 : Math.floor(r);
   return `https://raw.githubusercontent.com/brewcold/blog/refs/heads/main/_backgrounds/brewcold-bg-${num}.png`;
 };
