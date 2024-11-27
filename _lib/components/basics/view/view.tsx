5d865b20b26f8cb863291890a061f436a3453bcc
brewcold <seung.yoon.yu@gmail.com>
2024-11-27 17:53:02 +0900

    feat: view 컴포넌트 프롭 타입 변경

--- _lib/components/basics/view/view.tsx
+++ _lib/components/basics/view/view.tsx
@@ -1,18 +1,20 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import { ComponentPropsWithRef, ElementType, forwardRef, ReactNode, Ref } from 'react';
 import { BASE } from './view.css';
 
 type ViewProps<T extends ElementType> = {
   as?: T;
   styleVariant?: string;
   children: ReactNode;
-  //TODO: FLEX/GRID OPTIONS
-} & ComponentPropsWithoutRef<T>;
-export function View<T extends ElementType>({ as, styleVariant, children, ...props }: ViewProps<T>) {
+} & ComponentPropsWithRef<T>;
+
+function V<T extends ElementType>({ as, styleVariant, children, ...props }: ViewProps<T>, ref: Ref<HTMLElement>) {
   const Component = as || 'div';
   const className = styleVariant || BASE;
   return (
-    <Component className={className} {...props}>
+    <Component className={props.className || className} ref={ref} {...props}>
       {children}
     </Component>
   );
 }
+
+export const View = forwardRef(V);
