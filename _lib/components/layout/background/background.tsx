dc3eaa2183e51973a7797162c3096f2ef886a175
brewcold <seung.yoon.yu@gmail.com>
2024-12-23 15:20:00 +0900

    feat: 7.5초마다 배경 사진이 변화하도록 변경

--- _lib/components/layout/background/background.tsx
+++ _lib/components/layout/background/background.tsx
@@ -1,17 +1,17 @@
 'use client';
 import { LAYOUT_BG } from '@/app/page.css';
 import { View } from '../../basics/view/view';
-import { useRandomBackgroundImg } from '@/utils/get-background-img';
+import { useBackgroundTransition } from '@/utils/get-background-img';
 import { ComponentPropsWithoutRef } from 'react';
 
 export function BackgroundLayout({ ...props }: ComponentPropsWithoutRef<'div'>) {
-  const img = useRandomBackgroundImg(26);
+  const src = useBackgroundTransition(26);
 
   return (
     <View
-      stylevariant={props.className ?? LAYOUT_BG}
+      styleVariant={props.className ?? LAYOUT_BG}
       style={{
-        backgroundImage: `url('${img}') !important`,
+        backgroundImage: `url('${src}')`,
       }}
       {...props}>
       {props.children}
--- utils/get-background-img.ts
+++ utils/get-background-img.ts
@@ -1,9 +1,30 @@
 import { usePathname } from 'next/navigation';
-import { useRandomInt } from './use-random-int';
+import { getRandomInt, useRandomInt } from './use-random-int';
+import { useEffect, useState } from 'react';
+import { useIsomorphicLayoutEffect } from '@syyu/util/react';
 
 export const useRandomBackgroundImg = (nums_of_files: number): string => {
   const path = usePathname();
-  const n = useRandomInt([0, nums_of_files], [path]);
+  const n = useRandomInt([0, nums_of_files - 1], [path]);
+
+  return `https://raw.githubusercontent.com/brewcold/blog/refs/heads/main/_backgrounds/brewcold-bg-${n}.png`;
+};
+
+export const useBackgroundTransition = (nums_of_files: number) => {
+  const path = usePathname;
+  const [n, setNum] = useState<number>(0);
+
+  useIsomorphicLayoutEffect(() => {
+    setNum(getRandomInt([0, nums_of_files - 1]));
+  }, [path]);
+
+  useEffect(() => {
+    const T: ReturnType<typeof setTimeout> = setInterval(() => {
+      setNum(getRandomInt([0, nums_of_files - 1]));
+    }, 7500);
+
+    return () => clearTimeout(T);
+  }, []);
 
   return `https://raw.githubusercontent.com/brewcold/blog/refs/heads/main/_backgrounds/brewcold-bg-${n}.png`;
 };
