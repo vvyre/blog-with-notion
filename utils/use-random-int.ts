3d63d7017ec931478b0ad97a0b6c39f7f5b3a9fd
brewcold <seung.yoon.yu@gmail.com>
2024-12-23 15:15:20 +0900

    feat: 범위에 따른 랜덤한 정수를 반환하는 유틸 함수 추가

--- utils/use-random-int.ts
+++ utils/use-random-int.ts
@@ -3,6 +3,12 @@ import { DependencyList, useEffect, useState } from 'react';
 
 const randomInt = (num: number) => Math.floor(Math.random() * num);
 
+export const getRandomInt = (range: [number, number]): number => {
+  const [low, high] = range[0] < range[1] ? [range[0], range[1]] : [range[1], range[0]];
+
+  return randomInt(high - low + 1) + low;
+};
+
 export const useRandomInt = (range: [number, number], deps: DependencyList, init?: number): number => {
   const [low, high] = range[0] < range[1] ? [range[0], range[1]] : [range[1], range[0]];
 
