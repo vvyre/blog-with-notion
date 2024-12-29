a348655ec597a0c340ad5658b3c0ba027a6bf682
brewcold <seung.yoon.yu@gmail.com>
2024-12-29 17:50:46 +0900

    refactor: logo 컴포넌트 가독성 개선

--- _lib/components/layout/_external-logos/github.tsx
+++ _lib/components/layout/_external-logos/github.tsx
@@ -4,15 +4,30 @@ import { Btn } from '../../basics/button/btn';
 import MARK_WHITE from '#/img/github-mark-white.svg';
 import MARK_BLACK from '#/img/github-mark.svg';
 import { EXTERNAL_LINK, MENU_GITHUB_IMG } from '../navigation/about/about-btn.css';
-import { useSystemTheme } from '@/_lib/react/use-system-theme';
+import { useContext } from 'react';
+import { usePathname } from 'next/navigation';
+import { ThemeContext } from '@/_lib/context/theme-provider';
 
 export function GithubLink() {
-  const [theme, isPost] = useSystemTheme();
-  const MARK = theme === 'dark' ? MARK_BLACK : MARK_WHITE;
+  const { theme } = useContext(ThemeContext);
+  const path = usePathname();
+  const isPost = path.startsWith('engineering') || path.startsWith('til/');
 
-  return !isPost ? (
-    <Btn as="a" href="https://github.com/brewcold" target="_blank" className={EXTERNAL_LINK}>
-      <Image className={MENU_GITHUB_IMG} alt="brewcold github" src={MARK} />
-    </Btn>
-  ) : null;
+  if (isPost) return <></>;
+  else
+    switch (theme) {
+      case 'dark':
+        return (
+          <Btn as="a" href="https://github.com/brewcold" target="_blank" className={EXTERNAL_LINK}>
+            <Image key={theme} className={MENU_GITHUB_IMG} alt="brewcold github" src={MARK_WHITE} />
+          </Btn>
+        );
+      case 'light': {
+        return (
+          <Btn as="a" href="https://github.com/brewcold" target="_blank" className={EXTERNAL_LINK}>
+            <Image key={theme} className={MENU_GITHUB_IMG} alt="brewcold github" src={MARK_BLACK} />
+          </Btn>
+        );
+      }
+    }
 }
