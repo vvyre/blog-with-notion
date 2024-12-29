701a4ae099268ee3a596478c4365d9ddd5e1425d
brewcold <seung.yoon.yu@gmail.com>
2024-12-29 17:51:33 +0900

    feat: 다크, 라이트 모드를 변경한 적이 없는 경우 시스템 테마를 중간에 변경되어도 사이트가 반응해 테마를 변경하도록 수정

--- _lib/context/theme-provider.tsx
+++ _lib/context/theme-provider.tsx
@@ -1,14 +1,13 @@
 import { ReactNode, createContext } from 'react';
 import { useSystemTheme } from '../react/use-system-theme';
 
-export const ThemeContext = createContext<{ theme: string; prefersDark: boolean; toggle: Function }>({
+export const ThemeContext = createContext<{ theme: string; toggle: Function }>({
   theme: 'light',
-  prefersDark: false,
   toggle: () => {},
 });
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, prefersDark, toggle] = useSystemTheme();
+  const [theme, toggle] = useSystemTheme();
 
-  return <ThemeContext.Provider value={{ theme, prefersDark, toggle }}>{children}</ThemeContext.Provider>;
+  return <ThemeContext.Provider value={{ theme, toggle }}>{children}</ThemeContext.Provider>;
 }
--- _lib/react/use-system-theme.ts
+++ _lib/react/use-system-theme.ts
@@ -4,40 +4,46 @@ import { isClient } from '@syyu/util';
 
 type THEME = 'light' | 'dark';
 
-export const useSystemTheme = (): [THEME, boolean, () => void] => {
+export const useSystemTheme = (): [THEME, () => void] => {
   const [theme, setTheme] = useState<THEME>('light');
-  const [prefersDark, setPrefersDark] = useState(false);
 
-  const setLight = useCallback(() => {
+  const setLight = () => setTheme('light');
+
+  const setDark = () => setTheme('dark');
+
+  const setLightWithStorage = () => {
     setTheme('light');
-  }, []);
+    localStorage.setItem('bc_blog_theme', 'light');
+  };
 
-  const setDark = useCallback(() => {
+  const setDarkWithStorage = () => {
     setTheme('dark');
-  }, []);
+    localStorage.setItem('bc_blog_theme', 'dark');
+  };
 
   const toggle = useCallback(() => {
-    theme === 'light' ? setDark() : setLight();
+    theme === 'light' ? setDarkWithStorage() : setLightWithStorage();
   }, [theme]);
 
+  //시스템 설정을 따르는 부분
   useIsomorphicLayoutEffect(() => {
     if (isClient() && typeof localStorage !== 'undefined') {
       const storedTheme = localStorage.getItem('bc_blog_theme') as THEME;
-      const devicePrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-      setPrefersDark(devicePrefersDark);
-
       if (storedTheme) setTheme(storedTheme);
-      else if (devicePrefersDark) setDark();
+
+      const deviceThemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      const handleThemeChange = (e: MediaQueryListEvent) =>
+        //스토리지에 저장된 아이템이 없으면 시스템을 따릅니다
+        !localStorage.getItem('bc_blog_theme') && e.matches ? setDark() : setLight();
+      deviceThemeMediaQuery.addEventListener('change', handleThemeChange);
+
+      return () => deviceThemeMediaQuery.removeEventListener('change', handleThemeChange);
     }
   }, []);
 
   useEffect(() => {
-    if (isClient() && typeof localStorage !== 'undefined') {
-      localStorage.setItem('bc_blog_theme', theme);
-      document.body.dataset.theme = theme;
-    }
+    if (isClient() && typeof localStorage !== 'undefined') document.body.dataset.theme = theme;
   }, [theme]);
 
-  return useMemo(() => [theme, prefersDark, toggle], [theme, prefersDark, toggle]);
+  return useMemo(() => [theme, toggle], [theme, toggle]);
 };
