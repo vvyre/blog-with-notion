003bc875cdbb63b70c462ac0d697ec1267c5933b
brewcold <seung.yoon.yu@gmail.com>
2025-01-04 13:56:05 +0900

    fix: merge commits

--- _lib/react/use-category.ts
+++ _lib/react/use-category.ts
@@ -3,13 +3,8 @@ import { usePathname } from 'next/navigation';
 export const useCategory: () => { path: string; isEngineering: boolean; isStudy: boolean; isPost: boolean } = () => {
   const path = usePathname();
   const isEngineering = path.startsWith('/engineering');
-<<<<<<< HEAD
   const isStudy = path.startsWith('/study');
-  const isPost = isEngineering ?? (isStudy && path.length > 4);
-=======
-  const isTil = path.startsWith('/til');
-  const isPost = isEngineering || path.startsWith('/til/');
->>>>>>> d0a565f2d511496cd685bfdad7ee777bf34ba2c2
+  const isPost = isEngineering || path.startsWith('/study/');
 
   return { path, isEngineering, isStudy, isPost };
 };
