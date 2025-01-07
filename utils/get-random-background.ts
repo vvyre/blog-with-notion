e5546cea3086ea85e56e393250954811fe026f02
brewcold <seung.yoon.yu@gmail.com>
2025-01-07 17:14:42 +0900

    feat: path가 변할 때마다 study의 배경색이 변하도록 변경

--- utils/get-random-background.ts
+++ utils/get-random-background.ts
@@ -2,6 +2,7 @@ import { vars } from '@/_lib/styles/themes.css';
 import { objectKeys } from '@syyu/util';
 import { getRandomInt } from './get-random-int';
 import { useRandomNumber } from '@syyu/util/react';
+import { useCategory } from '@/_lib/react/use-category';
 
 const N = objectKeys(vars.notion).length;
 const num = getRandomInt([0, N]);
@@ -10,7 +11,8 @@ export const getRandomBackground = {
 };
 
 export const useRandomBackground = () => {
-  const P = useRandomNumber([0, N], [], 'int');
+  const { path } = useCategory();
+  const P = useRandomNumber([0, N], [path], 'int');
 
   return {
     backgroundColor: vars.notion[objectKeys(vars.notion)[P]],
