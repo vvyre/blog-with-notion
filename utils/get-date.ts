48a7a538f42f46e6ffdec28b733fdf68e67d69cb
brewcold <seung.yoon.yu@gmail.com>
2025-01-12 18:39:47 +0900

    feat: 작성 날짜를 월까지 표기하도록 변경

--- utils/get-date.ts
+++ utils/get-date.ts
@@ -1,6 +1,6 @@
 import type { PageObject } from '../_lib/types/notion-response';
 
 export function getDate(post: PageObject) {
-  const result = post.properties.date.date?.start;
+  const result = post.properties.date.date?.start?.slice(0, -3);
   return result ? result.split('-').join('. ') : '아직 작성 중인 글이에요.';
 }
