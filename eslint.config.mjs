e188a3d1be59130016ac1b23ed979cf0b1c09df1
brewcold <seung.yoon.yu@gmail.com>
2025-01-16 17:45:41 +0900

    chore: 로컬 eslint 및 prettier 설치 및 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  {
+    plugins: [pluginPrettier],
+    extends: ['plugin:prettier/recommended', configPrettier],
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]
