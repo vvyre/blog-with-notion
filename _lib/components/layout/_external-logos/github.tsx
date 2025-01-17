8530e610863df90768646ac552444580989ebc11
brewcold <seung.yoon.yu@gmail.com>
2025-01-17 17:12:25 +0900

    feat: 내비게이션의 두 버튼 추가

--- _lib/components/layout/_external-logos/github.tsx
+++ _lib/components/layout/_external-logos/github.tsx
@@ -7,51 +7,40 @@ import {
   EXTERNAL_LINK,
   MENU_GITHUB_IMG,
 } from '../navigation/about/about-btn.css'
-import { useContext } from 'react'
-import { usePathname } from 'next/navigation'
-import { BackgroundContext } from '@/_lib/context/background-provider'
+
+import { useBackgroundStore } from '../background/store'
+import { useEffect, useRef } from 'react'
 
 export function GithubLink() {
-  const { DARK_TEXT_PREFERED } = useContext(BackgroundContext)
-  const path = usePathname()
-  const isPost = path.startsWith('engineering') || path.startsWith('til/')
-  const theme = DARK_TEXT_PREFERED ? 'light' : 'dark'
+  const { PREFERS_DARK_TEXT, addRef } = useBackgroundStore()
 
-  if (isPost) return <></>
-  else
-    switch (theme) {
-      case 'dark':
-        return (
-          <Btn
-            as="a"
-            href="https://github.com/brewcold"
-            target="_blank"
-            className={EXTERNAL_LINK}
-          >
-            <Image
-              key={theme}
-              className={MENU_GITHUB_IMG}
-              alt="brewcold github"
-              src={MARK_WHITE}
-            />
-          </Btn>
-        )
-      case 'light': {
-        return (
-          <Btn
-            as="a"
-            href="https://github.com/brewcold"
-            target="_blank"
-            className={EXTERNAL_LINK}
-          >
-            <Image
-              key={theme}
-              className={MENU_GITHUB_IMG}
-              alt="brewcold github"
-              src={MARK_BLACK}
-            />
-          </Btn>
-        )
-      }
-    }
+  const githubRef = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    addRef('githubRef', githubRef)
+  }, [githubRef])
+
+  return (
+    <Btn
+      ref={githubRef}
+      as="a"
+      href="https://github.com/brewcold"
+      target="_blank"
+      className={EXTERNAL_LINK}
+    >
+      {PREFERS_DARK_TEXT() ? (
+        <Image
+          className={MENU_GITHUB_IMG}
+          alt="brewcold github"
+          src={MARK_WHITE}
+        />
+      ) : (
+        <Image
+          className={MENU_GITHUB_IMG}
+          alt="brewcold github"
+          src={MARK_BLACK}
+        />
+      )}
+    </Btn>
+  )
 }
--- _lib/components/layout/navigation/about/about-btn.tsx
+++ _lib/components/layout/navigation/about/about-btn.tsx
@@ -1,8 +1,23 @@
+'use client'
+import { useEffect, useRef } from 'react'
 import { Btn } from '../../../basics/button/btn'
 import { LOGO } from './about-btn.css'
+import { useBackgroundStore } from '../../background/store'
 export function AboutBtn({ ...props }) {
+  const profileButtonRef = useRef<HTMLButtonElement>(null)
+  const addRef = useBackgroundStore(state => state.addRef)
+
+  useEffect(() => {
+    addRef('profileBtn', profileButtonRef)
+  }, [profileButtonRef.current])
+
   return (
-    <Btn className={LOGO} aria-label="About Me" {...props}>
+    <Btn
+      ref={profileButtonRef}
+      className={LOGO}
+      aria-label="About Me"
+      {...props}
+    >
       Brewcold
     </Btn>
   )
