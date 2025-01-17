8964faa9d94cea33808b9c55cf11f26bc308dc01
brewcold <seung.yoon.yu@gmail.com>
2025-01-17 17:08:09 +0900

    delete: 배경 관련 기존 context 삭제

--- _lib/components/providers.tsx
+++ _lib/components/providers.tsx
@@ -4,18 +4,15 @@ import { PostListProvider } from '../context/post-list-provider'
 import { CurrentPostProvider } from '../context/current-post-provider'
 import { NavigationProvider } from '../context/navigation-provider'
 import { ThemeProvider } from '../context/theme-provider'
-import { BackgroundProvider } from '../context/background-provider'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider>
-      <BackgroundProvider>
-        <NavigationProvider>
-          <PostListProvider>
-            <CurrentPostProvider>{children}</CurrentPostProvider>
-          </PostListProvider>
-        </NavigationProvider>
-      </BackgroundProvider>
+      <NavigationProvider>
+        <PostListProvider>
+          <CurrentPostProvider>{children}</CurrentPostProvider>
+        </PostListProvider>
+      </NavigationProvider>
     </ThemeProvider>
   )
 }
