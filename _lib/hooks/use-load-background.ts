321ca1a1dcee0de2f9a7d851350a4c8b16e28818
brewcold <seung.yoon.yu@gmail.com>
2025-01-22 16:48:57 +0900

    feat: 기존 useLoadBackground를 useIntervalBackground로 이름 변경하고, useLoadbackground는 초기에 랜덤한 한 장의 배경 사진만 로드하도록 변경

--- _lib/hooks/use-load-background.ts
+++ _lib/hooks/use-load-background.ts
@@ -3,32 +3,35 @@ import { useEffect, useState } from 'react'
 import { useIsomorphicLayoutEffect } from '@syyu/util/react'
 import { getRandomInt } from '@/utils/get-random-int'
 import { preload } from 'react-dom'
+import { getRandomNumber } from '@syyu/util'
+import { useRandomInt } from './use-random-int'
 
 export const useLoadBackground = (nums_of_files: number): string => {
-  const path = usePathname()
-  const [num, setNum] = useState<number>(0)
-  const [next, setNext] = useState<number>(0)
+  // const path = usePathname()
+  // const [num, setNum] = useState<number>(getRandomNumber([0,nums_of_files-1], 'int'))
+  const num = useRandomInt([0, nums_of_files - 1], [nums_of_files])
+  // const [next, setNext] = useState<number>(0)
   const url = `https://raw.githubusercontent.com/brewcold/blog/refs/heads/main/_backgrounds/brewcold-bg-${num}.webp`
-  const nextUrl = `https://raw.githubusercontent.com/brewcold/blog/refs/heads/main/_backgrounds/brewcold-bg-${next}.webp`
+  // const nextUrl = `https://raw.githubusercontent.com/brewcold/blog/refs/heads/main/_backgrounds/brewcold-bg-${next}.webp`
 
-  useIsomorphicLayoutEffect(() => {
-    setNum(getRandomInt([0, nums_of_files - 1]))
-  }, [path])
+  // useIsomorphicLayoutEffect(() => {
+  //   setNum(getRandomInt([0, nums_of_files - 1]))
+  // }, [path])
 
-  useEffect(() => {
-    const T: ReturnType<typeof setTimeout> = setInterval(() => {
-      setNum(next)
-      setNext(getRandomInt([0, nums_of_files - 1]))
-    }, 10000)
+  // useEffect(() => {
+  //   const T: ReturnType<typeof setTimeout> = setInterval(() => {
+  //     setNum(next)
+  //     setNext(getRandomInt([0, nums_of_files - 1]))
+  //   }, 10000)
 
-    return () => clearInterval(T)
-  }, [next, nums_of_files])
-
-  useEffect(() => {
-    if (!(path === '/')) return
-    preload(url, { as: 'image' })
-    preload(nextUrl, { as: 'image' })
-  }, [url, nextUrl])
+  //   return () => clearInterval(T)
+  // }, [next, nums_of_files])
+  preload(url, { as: 'image' })
+  // useEffect(() => {
+  //   if (!(path === '/')) return
+  //   preload(url, { as: 'image' })
+  //   // preload(nextUrl, { as: 'image' })
+  // }, [url, nextUrl])
 
   return url
 }
