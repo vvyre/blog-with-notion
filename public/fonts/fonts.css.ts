19a314315abfeb1a26c7d544011015ec292e387d
brewcold <seung.yoon.yu@gmail.com>
2025-01-29 15:43:37 +0900

    fix: 확장자 오자 수정

--- public/fonts/fonts.css.ts
+++ public/fonts/fonts.css.ts
@@ -34,27 +34,27 @@ export const SANS_SERIF_SYNTHESIZED = fontFace([
 ])
 
 export const MONOSPACE_SYNTHESIZED = fontFace([
-  { src: 'url(/fonts/code-400.ttf)', fontWeight: 400 },
+  { src: 'url(/fonts/code-400.woff2)', fontWeight: 400 },
   {
-    src: 'url(/fonts/code-400-it.ttf)',
+    src: 'url(/fonts/code-400-it.woff2)',
     fontWeight: 400,
     fontStyle: 'italic',
   },
-  { src: 'url(/fonts/code-500.ttf)', fontWeight: 500 },
+  { src: 'url(/fonts/code-400.woff2)', fontWeight: 500 },
   {
-    src: 'url(/fonts/code-500-it.ttf)',
+    src: 'url(/fonts/code-400-it.woff2)',
     fontWeight: 500,
     fontStyle: 'italic',
   },
-  { src: 'url(/fonts/code-700.ttf)', fontWeight: 600 },
+  { src: 'url(/fonts/code-700.woff2)', fontWeight: 600 },
   {
-    src: 'url(/fonts/code-700-it.ttf)',
+    src: 'url(/fonts/code-700-it.woff2)',
     fontWeight: 600,
     fontStyle: 'italic',
   },
-  { src: 'url(/fonts/code-700.ttf)', fontWeight: 700 },
+  { src: 'url(/fonts/code-700.woff2)', fontWeight: 700 },
   {
-    src: 'url(/fonts/code-700-it.ttf)',
+    src: 'url(/fonts/code-700-it.woff2)',
     fontWeight: 700,
     fontStyle: 'italic',
   },
