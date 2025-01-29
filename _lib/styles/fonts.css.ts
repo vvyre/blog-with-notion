03178140af8b16ca81e660ced0589e380190344e
brewcold <seung.yoon.yu@gmail.com>
2025-01-29 15:33:28 +0900

    feat: 본문 폰트 변경

--- _lib/styles/fonts.css.ts
+++ _lib/styles/fonts.css.ts
@@ -1,4 +1,7 @@
-import { MONOSPACE_SYNTHESIZED } from '@/public/fonts/fonts.css'
+import {
+  MONOSPACE_SYNTHESIZED,
+  SANS_SERIF_SYNTHESIZED,
+} from '@/public/fonts/fonts.css'
 
-export const SANS_SERIF = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
+export const SANS_SERIF = `${SANS_SERIF_SYNTHESIZED}, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
 export const MONOSPACE = `${MONOSPACE_SYNTHESIZED}, monospace`
--- public/fonts/fonts.css.ts
+++ public/fonts/fonts.css.ts
@@ -1,5 +1,38 @@
 import { fontFace } from '@vanilla-extract/css'
 
+export const SANS_SERIF_SYNTHESIZED = fontFace([
+  { src: 'url(/fonts/body-v.ttf)', fontWeight: 400 },
+  {
+    src: 'url(/fonts/body-v-it.ttf)',
+    fontWeight: 400,
+    fontStyle: 'italic',
+  },
+  { src: 'url(/fonts/body-v.ttf)', fontWeight: 500 },
+  {
+    src: 'url(/fonts/body-v-it.ttf)',
+    fontWeight: 500,
+    fontStyle: 'italic',
+  },
+  { src: 'url(/fonts/body-v.ttf)', fontWeight: 600 },
+  {
+    src: 'url(/fonts/body-v-it.ttf)',
+    fontWeight: 600,
+    fontStyle: 'italic',
+  },
+  { src: 'url(/fonts/body-v.ttf)', fontWeight: 700 },
+  {
+    src: 'url(/fonts/body-v-it.ttf)',
+    fontWeight: 700,
+    fontStyle: 'italic',
+  },
+  { src: 'url(/fonts/body-v.ttf)', fontWeight: 900 },
+  {
+    src: 'url(/fonts/body-v-it.ttf)',
+    fontWeight: 900,
+    fontStyle: 'italic',
+  },
+])
+
 export const MONOSPACE_SYNTHESIZED = fontFace([
   { src: 'url(/fonts/code-400.ttf)', fontWeight: 400 },
   {
