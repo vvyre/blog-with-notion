6c04e42a1b54f06422f837e402d657394da6151e
vvyre <seung.yoon.yu@gmail.com>
2025-06-04 12:26:52 +0900

    feat: 폰트셋 변경

--- _styles/fonts.css.ts
+++ _styles/fonts.css.ts
@@ -1,2 +1,3 @@
 export const SANS_SERIF = `"Mona Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
+export const TITLES = `"Hubot Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
 export const MONOSPACE = `ui-monospace, SFMono-Regular, SF Mono, "JetBrains Mono", Menlo, Consolas, Liberation Mono, , monospace`
---
+++ _styles/global-fonts.css.ts
@@ -0,0 +1,37 @@
+import { globalFontFace } from '@vanilla-extract/css'
+
+globalFontFace('Mona Sans', {
+  src: 'url("/fonts/MonaSans-Regular.woff2") format("woff2")',
+  fontWeight: '400',
+  fontStyle: 'normal',
+})
+
+globalFontFace('Mona Sans', {
+  src: 'url("/fonts/MonaSans-Bold.woff2") format("woff2")',
+  fontWeight: '700',
+  fontStyle: 'normal',
+})
+
+globalFontFace('Mona Sans', {
+  src: 'url("/fonts/MonaSans-SemiBold.woff2") format("woff2")',
+  fontWeight: '600',
+  fontStyle: 'normal',
+})
+
+globalFontFace('Mona Sans', {
+  src: 'url("/fonts/MonaSans-Medium.woff2") format("woff2")',
+  fontWeight: '500',
+  fontStyle: 'normal',
+})
+
+globalFontFace('Hubot Sans', {
+  src: 'url("/fonts/HubotSansExpanded-Mdeium.woff2") format("woff2")',
+  fontWeight: '400',
+  fontStyle: 'normal',
+})
+
+globalFontFace('Hubot Sans', {
+  src: 'url("/fonts/HubotSansExpanded-Bold.woff2") format("woff2")',
+  fontWeight: '700',
+  fontStyle: 'normal',
+})
