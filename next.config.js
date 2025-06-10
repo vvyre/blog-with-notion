f8374947f20ca06d41f7b22000965f65b16b6076
vvyre <seung.yoon.yu@gmail.com>
2025-06-11 00:13:47 +0900

    fix: next.config를 cjs로 변경

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
-
 const withVanillaExtract = createVanillaExtractPlugin()
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -12,13 +12,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'raw.githubusercontent.com',
-        port: '',
         pathname: '/**',
       },
     ],
