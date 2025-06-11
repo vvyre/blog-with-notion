828dcb3fedf17bc1c79891d5f4fe656824af8a34
vvyre <seung.yoon.yu@gmail.com>
2025-06-11 10:46:40 +0900

    chore: 의존성 버전 변경 및 next.config esm으로 롤백

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
+import type { NextConfig } from 'next'
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 const withVanillaExtract = createVanillaExtractPlugin()
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
