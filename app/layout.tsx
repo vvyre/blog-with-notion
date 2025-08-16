1e4362ac30dbd4c855e3f053b33c9963a6018181
vvyre <seung.yoon.yu@gmail.com>
2025-08-16 19:58:13 +0900

    fix: 내비게이션 삭제(임시

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default async function RootLayout({
     <html lang="ko">
       <View as="body">
         <Providers>
-          <Navigation />
+          {/* <Navigation /> */}
           <BackgroundControl imgSrcList={imgSrcList} />
           {children}
         </Providers>
